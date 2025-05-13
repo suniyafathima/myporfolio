88126e66c054cb6f46fe0547245402b3d1cd825b
suniyafathima <suniyafathima@gmail.com>
2025-05-13 15:39:07 +0530

    completed

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1481647528"
+    serviceWorkerVersion: "2948639100"
   }
 });
